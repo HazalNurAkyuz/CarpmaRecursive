de2c557c5d55e474085958af0ca56c82812ee4af
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-09-05 10:17:20 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+    public static int carpma(int x, int y) {
+        if(y==0) {
+            return 0;
+        }else {
+            return x + carpma(x,y-1);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println(carpma(3,5));
+
     }
 }
